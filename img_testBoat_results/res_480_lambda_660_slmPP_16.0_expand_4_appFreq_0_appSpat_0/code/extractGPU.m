d8598bb9198666237148a0039106f15be22df72f
Dongheon Yoo <39788884+dongheon-yoo@users.noreply.github.com>
2020-07-29 17:31:37 +0900

    delete

--- img_testBoat_results/res_480_lambda_660_slmPP_16.0_expand_4_appFreq_0_appSpat_0/code/extractGPU.m
+++
@@ -1,8 +0,0 @@
-function outArray = extractGPU(inArray)
-    %% Extract array from GPU to local device
-    if isa(inArray, 'gpuArray')
-        outArray = gather(inArray);
-    else
-        outArray = inArray;
-    end
-end
--- img_testBoat_results/res_960_lambda_660_slmPP_16.0_expand_4_appFreq_1_appSpat_0/code/convertGPU.m
+++
@@ -1,8 +0,0 @@
-function outGPU = convertGPU(input)
-    %% Check GPU compatibility
-    if canUseGPU
-        outGPU = gpuArray(input);
-    else
-        outGPU = input;
-    end
-end
